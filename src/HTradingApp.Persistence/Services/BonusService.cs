a342db6acc1efe8bd0b932ac5437fe7181ac2a3c
bigGeorgi-InDaHouse <86378722+bigGeorgi-InDaHouse@users.noreply.github.com>
2023-12-10 17:18:42 +0000

    Refactored code

--- src/HTradingApp.Persistence/Services/BonusService.cs
+++ src/HTradingApp.Persistence/Services/BonusService.cs
@@ -17,7 +17,12 @@ namespace HTradingApp.Persistence.Services
 
         public bool AddAccountBonusPoints(int accountId, int bonusPoints)
         {
-            List<BonusPoint> bonusPointList = (List<BonusPoint>)_cache.Get("BonusPoints");
+            List<BonusPoint>? bonusPointList = _cache.Get("BonusPoints") as List<BonusPoint>;
+
+            if (bonusPointList == null)
+            {
+                return false;
+            }
 
             if (bonusPointList.Any(x => x.AccountId == accountId))
             {
@@ -35,17 +40,15 @@ namespace HTradingApp.Persistence.Services
 
             bonusPointList.Add(newBonusPoint);
 
-            _cache.Set<List<BonusPoint>>("BonusPoints", bonusPointList);
+            _cache.Set("BonusPoints", bonusPointList);
 
-            List<BonusPoint> cacheUpdated = (List<BonusPoint>)_cache.Get("BonusPoints");
-            bool isAdded = cacheUpdated.Count() == bonusPointList.Count();
-
-            return isAdded;
+            List<BonusPoint>? cacheUpdated = _cache.Get("BonusPoints") as List<BonusPoint>;
+            return cacheUpdated?.Count == bonusPointList.Count;
         }
 
         public decimal CalculateAccountCredit(int bonusPoints)
         {
-            const decimal bonusMultiplier = 12.5M;
+            decimal bonusMultiplier = 12.5M;
 
             return Math.Round(bonusPoints * bonusMultiplier, 2);
         }
@@ -59,52 +62,63 @@ namespace HTradingApp.Persistence.Services
 
             int bonusPoints = 0;
             decimal dealsSum = deals.Select(x => x.Amount).Sum();
+
+
             if (deals.Count >= 10)
+            {
                 bonusPoints += 10;
+            }
 
-            if (dealsSum >= 1000)
-                bonusPoints += 5;
+            // Defining thresholds instead of if statements
+            Dictionary<decimal, int> bonusThresholds = new Dictionary<decimal, int>
+            {
+                { 10000000, 45 },
+                { 1000000, 30 },
+                { 100000, 20 },
+                { 10000, 10 },
+                { 1000, 5 }
+            };
 
-            if (dealsSum >= 10000)
-                bonusPoints += 10;
-
-            if (dealsSum >= 100000)
-                bonusPoints += 20;
-
-            if (dealsSum >= 1000000)
-                bonusPoints += 30;
-
-            if (dealsSum >= 10000000)
-                bonusPoints += 45;
+            foreach (var threshold in bonusThresholds)
+            {
+                if (dealsSum >= threshold.Key)
+                {
+                    bonusPoints += threshold.Value;
+                }
+            }
 
             return bonusPoints;
         }
 
         public bool FlushBonusPoints(int accountId)
         {
-            List<BonusPoint> bonusPoints = (List<BonusPoint>)_cache.Get("BonusPoints");
-            foreach (BonusPoint bonusPoint in bonusPoints)
+            if (!(_cache.Get("BonusPoints") is List<BonusPoint> bonusPoints))
             {
-                if (bonusPoint.ConvertedToCredit == false && bonusPoint.AccountId == accountId)
-                {
-                    bonusPoint.ConvertedToCredit = true;
-                }
+                return false;
+            }
+
+            foreach (var bonusPoint in bonusPoints
+                .Where(x => !x.ConvertedToCredit && x.AccountId == accountId))
+            {
+                bonusPoint.ConvertedToCredit = true;
             }
 
             _cache.Set("BonusPoints", bonusPoints);
 
-            List<BonusPoint> updatedList = (List<BonusPoint>)_cache.Get("BonusPoints");
-            return !updatedList.Any(x => x.ConvertedToCredit == false && x.AccountId == accountId);
+            return !bonusPoints.Any(x => !x.ConvertedToCredit && x.AccountId == accountId);
         }
 
         public int GetAccountBonusPoints(int accountId)
         {
-            var bonusPoints = (List<BonusPoint>)_cache.Get("BonusPoints");
-            var bonusPointsForAccount = bonusPoints
-                .Where(x => x.AccountId == accountId && x.ConvertedToCredit == false)
-                .Select(y => y.Amount);
+            var bonusPoints = _cache.Get("BonusPoints") as List<BonusPoint>;
+            if (bonusPoints == null)
+            {
+                return 0;
+            }
 
-            return bonusPointsForAccount.Sum();
+            return bonusPoints
+                .Where(x => x.AccountId == accountId && !x.ConvertedToCredit)
+                .Sum(bp => bp.Amount);
         }
 
         public bool IsEligibleForBonusPoints(int accountId, List<Deal> deals)
